16dd2d044d274b9ee13e47271e71ba57e4b87599
vonnue-assin <assin.anna@vonnue.com>
2025-12-04 12:17:49 +0530

    refactor : updated the code

--- src/components/DropDownSolutionsHubbo/index.tsx
+++ src/components/DropDownSolutionsHubbo/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { DropDownProps, DropdownItem } from "../../types";
+
 import "./styles.css";
 
 interface UnifiedDropdownProps extends DropDownProps {
